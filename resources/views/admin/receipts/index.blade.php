2a58a4111d0439900bc3fd09332fc43449b8bdc7
atefAkl <atefakl80@gmail.com>
2025-01-06 21:47:29 +0300

    رفع تعديلات البحث بالسيريال

--- resources/views/admin/receipts/index.blade.php
+++ resources/views/admin/receipts/index.blade.php
@@ -130,13 +130,13 @@
               @endforeach
             </select>
             <select class="form-select form-control sm py-0" name="admin_id" id="admin_id">
-              <option value="">Representative</option>
+              <option value="">Person</option>
               @foreach ($admins as $admin)
               <option {{isset($query['admin_id']) && $query['admin_id'] ==  $admin->id ?'selected':''}} value="{{ $admin->id }}">{{ $admin->userName }}</option>
               @endforeach
             </select>
             <select class="form-select form-control sm py-0" name="direction" id="direction">
-              <option value="0" >Direction</option>
+              <option value="0" >Dir</option>
               @foreach ($receipt_direction as $d_index => $direction)
               <option {{isset($query['direction']) && $query['direction'] == $d_index ?'selected':''}} value="{{$d_index}}">{{$direction}}</option>
 
@@ -161,96 +161,131 @@
           <thead>
             <tr>
               <th><i class="fa fa-list"></i></th>
-              <th><i class="fa fa-barcode"></i> Serial Number</th>
+              <th><i class="fa fa-barcode"></i> SN</th>
               <th><i class="fa fa-tags"></i> Ref Type</th>
               <th><i class="fa fa-calendar"></i> Date</th>
               <th><i class="fa fa-arrow-right"></i> Dir</th>
-              <th><i class="fa fa-user"></i> Representative</th>
+              <th><i class="fa fa-user"></i> Person</th>
               <th><i class="fa fa-check-circle"></i>Status</th>
               <th><i class="fa fa-cogs"></i> Control</th>
             </tr>
           </thead>
-          @php
-          $counter = 0;
-          @endphp
-          @foreach ($receipts as $receipt)
+          <tbody id="receipt-list">
+            @php
+            $counter = 0;
+            @endphp
+            @foreach ($receipts as $receipt)
 
-          <tr>
-            <td>{{ ++$counter }}</td>
-            <td><a class="btn btn-sm py-0 p-0" data-bs-toggle="tooltip"  title="Display Receipt" href="#">{{ $receipt->serial }}</a></td>
-            <td>{{ $receipt->getTypeName() }}</td>
-            <td>{{ $receipt->reception_date }}</td>
-            <td>
-              @if ($receipt->direction === 1)
-              <span class="badge bg-success">{{ $receipt_direction[$receipt->direction] }}</span>
-              @elseif($receipt->direction === 2)
-              <span class="badge bg-danger">{{ $receipt_direction[$receipt->direction] }}</span>
-              @endif
-            </td>
-            <td data-bs-toggle="tooltip" title="{{ 'Manager' }}" {{-- @$receipt->admin->profile->possition --}}>{{ $receipt->admin->userName }}</td>
-            <td>
-              @if($receipt->status === 1)
-              <span class="badge bg-secondary"> {{ $receipt_status[$receipt->status] }}</span>
-              @elseif($receipt->status === 2)
-              <span class="badge bg-info"> {{ $receipt_status[$receipt->status] }}</span>
-              @elseif($receipt->status === 3)
-              <span class="badge bg-warning"> {{ $receipt_status[$receipt->status] }}</span>
-              @endif
-            </td>
-            <td>
-              @if($receipt->status == 1)
-              <a class="btn btn-sm py-0 p-0" data-bs-toggle="tooltip" title="edit Receipt"
-                href="{{ route('edit-receipt-info', [$receipt->id]) }}"><i
-                  class="fa fa-edit text-primary"></i></a>
-              <a class="btn btn-sm py-0 p-0" data-bs-toggle="tooltip" title="Approve Receipt"
-              href="{{ route('approve-receipt', [$receipt->id]) }}"><i class="fa fa-check text-info"></i></a>
-              @php
-              $addEntry =
-              $receipt->direction === 1 ? 'add-store-input-entry' : 'add-store-output-entry';
-              @endphp
-              <a class="btn btn-sm py-0 p-0" data-bs-toggle="tooltip" title="Add Entries"
-                href="{{ route($addEntry, [$receipt->id]) }}"><i
-                  class="fa fa-square-plus text-success"></i></a>
+            <tr>
+              <td>{{ ++$counter }}</td>
+              <td><a class="btn btn-sm py-0 p-0" data-bs-toggle="tooltip"  title="Display Receipt" href="#">{{ $receipt->serial }}</a></td>
+              <td>{{ $receipt->getTypeName() }}</td>
+              <td>{{ $receipt->reception_date }}</td>
+              <td>
+                @if ($receipt->direction === 1)
+                <span class="badge bg-success">{{ $receipt_direction[$receipt->direction] }}</span>
+                @elseif($receipt->direction === 2)
+                <span class="badge bg-danger">{{ $receipt_direction[$receipt->direction] }}</span>
+                @endif
+              </td>
+              <td data-bs-toggle="tooltip" title="{{ 'Manager' }}" {{-- @$receipt->admin->profile->possition --}}>{{ $receipt->admin->userName }}</td>
+              <td>
+                @if($receipt->status === 1)
+                <span class="badge bg-secondary"> {{ $receipt_status[$receipt->status] }}</span>
+                @elseif($receipt->status === 2)
+                <span class="badge bg-info"> {{ $receipt_status[$receipt->status] }}</span>
+                @elseif($receipt->status === 3)
+                <span class="badge bg-warning"> {{ $receipt_status[$receipt->status] }}</span>
+                @endif
+              </td>
+              <td>
+                @if($receipt->status == 1)
+                <a class="btn btn-sm py-0 p-0" data-bs-toggle="tooltip" title="edit Receipt"
+                  href="{{ route('edit-receipt-info', [$receipt->id]) }}"><i
+                    class="fa fa-edit text-primary"></i></a>
+                <a class="btn btn-sm py-0 p-0" data-bs-toggle="tooltip" title="Approve Receipt"
+                href="{{ route('approve-receipt', [$receipt->id]) }}"><i class="fa fa-check text-info"></i></a>
+                @php
+                $addEntry =
+                $receipt->direction === 1 ? 'add-store-input-entry' : 'add-store-output-entry';
+                @endphp
+                <a class="btn btn-sm py-0 p-0" data-bs-toggle="tooltip" title="Add Entries"
+                  href="{{ route($addEntry, [$receipt->id]) }}"><i
+                    class="fa fa-square-plus text-success"></i></a>
 
-              @elseif($receipt->status == 2)
-              <a class="btn btn-sm py-0 p-0" data-bs-toggle="tooltip" title="Enable Receipt Entries "
-                href=""><i class="fa fa-ban text-primary"></i></a>
-              <a class="btn btn-sm py-0 p-0" data-bs-toggle="tooltip"
-                title="archive Receipt"
-                onclick="if (!confirm('You are going to archive this receipt, are you sure?'))return false"
-                href="{{ route('archive-receipt', [$receipt->id]) }}"><i
-                  class="fa fa-archive text-danger"></i></a>
+                @elseif($receipt->status == 2)
+                <a class="btn btn-sm py-0 p-0" data-bs-toggle="tooltip" title="Enable Receipt Entries "
+                  href=""><i class="fa fa-ban text-primary"></i></a>
+                <a class="btn btn-sm py-0 p-0" data-bs-toggle="tooltip"
+                  title="archive Receipt"
+                  onclick="if (!confirm('You are going to archive this receipt, are you sure?'))return false"
+                  href="{{ route('archive-receipt', [$receipt->id]) }}"><i
+                    class="fa fa-archive text-danger"></i></a>
 
-              @elseif($receipt->status == 3)
-              <a class="btn btn-sm py-0 p-0" data-bs-toggle="tooltip" title="Restore Receipt"
-                onclick="if (!confirm('You are going to Restore this receipt, are you sure?'))return false"
-                href="{{ route('restore-receipt-info', [$receipt->id]) }}"><i
-                  class="fa fa-undo text-warning"></i></a>
-              @endif
-              <a class="btn btn-sm py-0 p-0" data-bs-toggle="tooltip" title="Q-Display Receipt" href=""><i
-                                    class="fa fa-file text-primary"></i></a>
-                            <a class="btn btn-sm py-0 p-0" data-bs-toggle="tooltip" title="print Receipt" href=""><i
-                                    class="fa fa-print text-secondary"></i></a>
-                            <a class="btn btn-sm py-0 p-0" data-bs-toggle="tooltip" title=" delete Receipt"
-                                onclick="if (!confirm('You are going to delete this receipt, are you sure?'))return false"
-                                href="{{ route('forceDelete-receipt-info', [$receipt->id]) }}"><i
-                                    class="fa fa-trash-alt text-danger"></i></a>
+                @elseif($receipt->status == 3)
+                <a class="btn btn-sm py-0 p-0" data-bs-toggle="tooltip" title="Restore Receipt"
+                  onclick="if (!confirm('You are going to Restore this receipt, are you sure?'))return false"
+                  href="{{ route('restore-receipt-info', [$receipt->id]) }}"><i
+                    class="fa fa-undo text-warning"></i></a>
+                @endif
+                <a class="btn btn-sm py-0 p-0" data-bs-toggle="tooltip" title="Q-Display Receipt" href=""><i
+                                      class="fa fa-file text-primary"></i></a>
+                              <a class="btn btn-sm py-0 p-0" data-bs-toggle="tooltip" title="print Receipt" href=""><i
+                                      class="fa fa-print text-secondary"></i></a>
+                              <a class="btn btn-sm py-0 p-0" data-bs-toggle="tooltip" title=" delete Receipt"
+                                  onclick="if (!confirm('You are going to delete this receipt, are you sure?'))return false"
+                                  href="{{ route('forceDelete-receipt-info', [$receipt->id]) }}"><i
+                                      class="fa fa-trash-alt text-danger"></i></a>
 
 
-            </td>
-          </tr>
-          @endforeach
-          <tbody>
+              </td>
+            </tr>
+            @endforeach
+          </tbody>
 
 
         </table>
-        <div class="mt-3">
-          {{ $receipts->links() }}
-        </div>
-    </fieldset>
+      </fieldset>
+      <div class="mt-3">
+        {{ $receipts->links() }}
+      </div>
   </div>
 </div>
 
+<script>
+  $(document).ready(function () {
+    //sn search btn onkeyup
+    $('#search').on('keyup', function () {
+      //get search form params // dir, status, admin_id, reference_type
+      // get search value form sn input
+      // ajaxRequest
+      var search = $(this).val();
+      var dir = $('#direction').val();
+      var status = $('#status').val();
+      var admin_id = $('#admin_id').val();
+      var reference_type = $('#reference_type').val();
+      var url = "{{ route('display-recepits-list') }}";
+      $.ajax({
+        url: url,
+        type: 'GET',
 
+        data: {
+          search,
+          dir,
+          status,
+          admin_id,
+          reference_type
+        },
+        success: function (response) {
+          const receiptData = JSON.parse(response).map();
+          $('#receipt-list').html(response);
+        },
+        error: function (xhr, status, error) {
+          console.log(error);
+        }
+      });
+    });
+  });
+</script>
 
 @endsection
