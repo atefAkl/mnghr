c1961922a85e545008535f76c0c56591c6c5796b
atefAkl <atefakl80@gmail.com>
2025-01-29 17:24:57 +0300

    upload temolate

---
+++ resources/views/layouts/report.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    {{-- header --}}
+        {{-- Left Side --}}
+            {{-- Company Name --}}
+            {{-- Company Logo --}}
+            {{-- Company Scope --}}
+            {{-- Company CR Number --}}
+        {{-- Right Side --}}
+            {{-- QR Code --}}
+            
+        {{-- center --}}
+            {{-- Report Title --}}
+            {{-- Report Date --}}
+            {{-- Report pages --}}
+    {{-- body --}}
+        {{-- parts of reports --}}
+            table
+        {{-- Content --}}
+            table or text
+        {{-- Signatures --}}
+    {{-- footer --}}
+        {{-- address --}}
+        {{-- connections --}}
+            
+</body>
+</html>
